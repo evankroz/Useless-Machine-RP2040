8335e8e365b93dfb162c08cd9b86cb0e178e8372
evankroz <117702363+evankroz@users.noreply.github.com>
2024-03-12 09:04:16 -0400

    Main Code

---
+++ code.py
@@ -0,0 +1,51 @@
+import random
+import time
+import board
+import pwmio
+import digitalio
+from adafruit_motor import servo
+
+
+pwm_servo12= pwmio.PWMOut(board.GP12, frequency=50)
+servo12 = servo.Servo(pwm_servo12)
+pwm_servo15 = pwmio.PWMOut(board.GP15, frequency=50)
+servo15 = servo.Servo(pwm_servo15)
+switch = digitalio.DigitalInOut(board.GP0)
+switch.direction = digitalio.Direction.INPUT
+switch.pull = digitalio.Pull.DOWN
+
+servo12.angle = 0
+servo15.angle = 15
+
+def move(servo, target_angle, delay = 0.01, speed = 10):
+    step = speed if target_angle > servo.angle else -speed
+    for angle in range(int(servo.angle), target_angle, step):
+        servo.angle = angle
+        time.sleep(delay)
+
+#main code here
+while True:
+    if switch.value:
+        count = 0
+        while count < 7:
+            move(servo15, 115, delay=0.01, speed=4)
+            move(servo12, 135, delay=0.01, speed=15)
+            move(servo12, 0, delay=0.01, speed=15)
+            time.sleep(0.01)
+            count += 1
+        move(servo15, 115, delay=0.01, speed=4)
+        move(servo12, 90, delay=0.05, speed=5)
+        move(servo12, 180, delay=0.01, speed=15)
+        time.sleep (0.5)
+        while switch.value:
+            move(servo12, 0, delay=0.01, speed=10)
+            servo12.angle = 180
+            time.sleep(0.5)
+
+            move(servo15, 0, delay=0.01, speed=10)
+            time.sleep(0.2)
+            move(servo15, 15, delay=0.01, speed=10)
+            time.sleep(0.2)
+        else:
+            move(servo12, 0, delay=0.02)
+            servo15.angle = 15
